1f2d5f6efeb4b2fb95b344b4d0aa0de035db1fee
ahmed <rinxdz75@gmail.com>
2023-12-03 20:55:22 +0100

    feat: add django all auth and rest

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("main.urls")),
+    path('api/auth/', include('dj_rest_auth.urls')),
+    path('api/auth/registration/', include('dj_rest_auth.registration.urls')),
 ]
