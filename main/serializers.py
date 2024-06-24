3eef73504c8b8221ee4b485d2dc1b1fb9360531e
bernous <bernous@gmail.com>
2024-06-25 00:29:13 +0100

    version 12:30

--- main/serializers.py
+++ main/serializers.py
@@ -1,7 +1,16 @@
 # views.py
 from rest_framework import serializers
 from rest_framework.permissions import AllowAny
-from .models import User, Schedule, TimeSlot, Module, Classroom, Teacher, Planning
+from .models import (
+    User,
+    Schedule,
+    TimeSlot,
+    Module,
+    Classroom,
+    Teacher,
+    Planning,
+    Group,
+)
 from dj_rest_auth.serializers import LoginSerializer
 from django.contrib.auth.hashers import make_password
 
@@ -24,20 +33,60 @@ class CustomLoginSerializer(LoginSerializer):
 
 
 class TeacherSerializer(serializers.ModelSerializer):
+    planning = serializers.PrimaryKeyRelatedField(
+        queryset=Planning.objects.all(), many=True
+    )
+    modules = serializers.PrimaryKeyRelatedField(
+        queryset=Module.objects.all(), many=True
+    )
 
     class Meta:
         model = Teacher
         fields = "__all__"
+
+    def create(self, validated_data):
+        planning_data = validated_data.pop("planning", [])
+        module_data = validated_data.pop("modules", [])
+
+        teacher = Teacher.objects.create(**validated_data)
+
+        # Add planning
+        teacher.planning.set(planning_data)
+
+        # Add modules
+        teacher.modules.set(module_data)
+
+        return teacher
+
+    def update(self, instance, validated_data):
+        planning_data = validated_data.pop(
+            "planning", []
+        )  # Ensure planning_data is not None
+        instance.name = validated_data.get("name", instance.name)
+        instance.hours = validated_data.get("hours", instance.hours)
+        instance.save()
+        instance.planning.set(planning_data)  # Update many-to-many relationships
+        return instance
 
 
 class ModuleSerializer(serializers.ModelSerializer):
     teachers = serializers.PrimaryKeyRelatedField(
         many=True, queryset=Teacher.objects.all(), required=False
     )
+    planning = serializers.PrimaryKeyRelatedField(
+        queryset=Planning.objects.all(), many=True
+    )
 
     class Meta:
         model = Module
         fields = "__all__"
+
+    def create(self, validated_data):
+        planning_data = validated_data.pop("planning")
+        module = Module.objects.create(**validated_data)
+        for planning_id in planning_data:
+            module.planning.add(planning_id)
+        return module
 
 
 class ClassroomSerializer(serializers.ModelSerializer):
@@ -45,6 +94,13 @@ class ClassroomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Classroom
         fields = "__all__"
+
+    def create(self, validated_data):
+        planning_data = validated_data.pop("planning")
+        classroom = Classroom.objects.create(**validated_data)
+        for planning_id in planning_data:
+            classroom.planning.add(planning_id)
+        return classroom
 
 
 class TimeSlotSerializer(serializers.ModelSerializer):
@@ -78,4 +134,11 @@ class ClassroomSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Classroom
+        fields = "__all__"
+
+
+class GroupSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Group
         fields = "__all__"
